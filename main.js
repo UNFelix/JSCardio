ae8695035ab4564acb94aed25ed41c80052b48b1
unfelix <shevko.evheniy@gmail.com>
2020-12-01 13:03:31 +0200

    simple timer

--- main.js
+++ main.js
@@ -1,11 +1,65 @@
-const openModalBtn = document.getElementById('button')
-const closeBtn = document.getElementById('close')
-const submitBtn = document.getElementById('submit')
-const modalDialog = document.getElementById('modal')
-const modalForm = document.forms[0]
+const startBtn = document.getElementById('startBtn')
+const stopBtn = document.getElementById('stopBtn')
+const pauseBtn = document.getElementById('pauseBtn')
+const resumeBtn = document.getElementById('resumeBtn')
+const timer = document.getElementById('timer')
 
+let time = 60
+let interval
+let onFinish
+let status
 
-openModalBtn.onclick = () => modalDialog.showModal()
-closeBtn.onclick = () => modalDialog.close()
-// modalForm.onsubmit = () => modalForm.classList.add('checked')
-submitBtn.onclick = () => modalForm.classList.add('checked')
+startBtn.onclick = startTimer
+stopBtn.onclick = stopTimer
+pauseBtn.onclick = pauseTimer
+resumeBtn.onclick = resumeTimer
+
+function drawTime() {
+  timer.innerText = time
+}
+
+function startTimer(){
+  status = 'paused'
+  time = 10
+  resumeTimer()
+}
+
+function stopTimer(){
+  if (status == 'stopped'){
+    return
+  }
+  status = 'stopped'
+  clearInterval(interval)
+  drawTime()
+  if (onFinish){
+    onFinish()
+  }
+}
+
+function pauseTimer(){
+  if (status != 'running'){
+    return
+  }
+  status = 'paused'
+  clearInterval(interval)
+  drawTime()
+}
+
+function resumeTimer(){
+  if (status != 'paused'){
+    return
+  }
+  status = 'running'
+  drawTime()
+  interval = setInterval(() => {
+    if(time <= 0) {
+      stopTimer()
+    }
+    else{
+      time--
+    }
+    drawTime()
+  }, 1000) 
+}
+
+onFinish = () => alert('timer stopped')
